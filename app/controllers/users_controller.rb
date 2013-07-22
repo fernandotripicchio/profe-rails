c602df89fa60e731974bfab41504b7acef0d87ee
Fernando Tripicchio <fernando.tripicchio@dreamstaffing.com>
2013-07-22 12:52:11 -0300

    agregue afiliados

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,15 @@ class UsersController < ApplicationController
   end
   
   def index
-    
+    @users = User.all
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_path }
+      format.json { head :no_content }
+    end  
+ end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Profe::Application.routes.draw do
   match 'login' => 'user_sessions#new', :as => :login
   match 'logout' => 'user_sessions#destroy', :as => :logout    
   root :to => "users#index"
+  
+  resources :afiliados
+  
 end
